ab9148e92ec7dbe3a0c3d06faa01b5fcf65887f3
enz54 <enz54@yahoo.co.uk>
2017-03-24 08:41:24 +0000

    UPDATED FLLE

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
-// var logger = function(req,res,next){
-//     console.log('Logging ...');
-//     next();
-// }
+/* var logger = function(req,res,next){
+     console.log('Logging ...');
+     next();
+ }
 
-// app.use(logger);
+ app.use(logger);
+ */
+
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -19,6 +22,26 @@ app.set('views', path.join(__dirname, 'views'));
 //Body-Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
 
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
@@ -49,6 +72,26 @@ app.get('/', function(req,res){
         title:'Customers',
         users:users
     });
+});
+
+app.post('/users/add', function(req,res){
+    req.checkBody('first_name', 'Firs tname is required').notEmpty();
+    req.checkBody('last_name', 'Last name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    
+    var errors = req.validationErrors(); 
+    if(errors){
+        console.log('ERRORS');
+    } else {
+    
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
+        }
+    console.log('SUCCESS');
+    }
+
 });
 
 app.listen(3000, function(){
